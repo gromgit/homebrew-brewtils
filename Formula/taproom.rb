ea90461ef2538b0253223f50e6781ddedd9c19c7
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-07-05 08:45:26 +0000

    taproom: add 0.1.0 bottle.

--- Formula/taproom.rb
+++ Formula/taproom.rb
@@ -5,6 +5,13 @@ class Taproom < Formula
   sha256 "176dc961af636cde024f52d10856423ca9f2e62d080300c7f393bec1824023c1"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/brewtils"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "1cec921e495133579524f8eade2df34b16d1741a48ba732e1b20833ec8987102"
+    sha256 cellar: :any_skip_relocation, ventura:      "9cf9dce908d5368c3c65bd581b343fe2f3578840e0212e6c21156c98007f3918"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "978d45ee223bf4b6394b71dc1763410c22262bed8ac55fba3a3a2d0573d0e611"
+  end
+
   depends_on "go" => :build
 
   def install
