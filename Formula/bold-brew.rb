891977a3da074371075b216ba9e20cf0b23af0d3
Adrian Ho <215702+gromgit@users.noreply.github.com>
2025-07-06 04:05:36 +0000

    bold-brew: add 1.8.0 bottle.

--- Formula/bold-brew.rb
+++ Formula/bold-brew.rb
@@ -5,6 +5,13 @@ class BoldBrew < Formula
   sha256 "de2e98fd8a961222eb4e0b28fbc05e5c8f322c4cc34d3919407cafbe863af772"
   license "MIT"
 
+  bottle do
+    root_url "https://ghcr.io/v2/gromgit/brewtils"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "4ef128a6aec4e62faad6553671daeb25b2a7dbdbba1f864c620838f516d59635"
+    sha256 cellar: :any_skip_relocation, ventura:      "4dcd7096735e348d04e9f999f5905be8202a33a3e1316f8efaa2401a20dbcb50"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "934b2ef4bf8ec901ab727ec8ed859ef84340b219713251ae27337ae13e4039c2"
+  end
+
   depends_on "go" => :build
 
   def install
